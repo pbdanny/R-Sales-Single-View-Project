603ede419e33710f9a504fce6ce11f116a668857
pbdanny <pb.danny@gmail.com>
2017-05-01 16:23:51 +0700

    Data cleanning and combine all.

--- Sales Single View-EDA.R
+++ Sales Single View-EDA.R
@@ -5,5 +5,8 @@
 # Data Loading
 
 sales2013 <- read.delim(file = "./Data all type team/DataAllTypeTeam2013", stringsAsFactors = FALSE)
+
+# Data Editing
+
 sales2013$TL_Code <- as.character(sales2013$TL_Code)
 sales2013$Agent_Code <- as.character(sales2013$Agent_Code)
